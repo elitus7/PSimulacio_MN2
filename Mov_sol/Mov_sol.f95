115044ca68b39158975ead295f3160d0b9effcf3
Erco-10 <erco9526@gmail.com>
2025-01-05 17:11:01 +0100

    arreglando cositas

--- Mov_sol/Mov_sol.f95
+++ Mov_sol/Mov_sol.f95
@@ -1,10 +1,11 @@
 program mov_sol
-    Integer :: N_a !Passos temporals (tot l'any)
-    Integer :: N_d !Passos temporals (tot el dia)
-    Real, dimension(N_a) :: Theta !Angle d'incidencia solar que depén de l'alçada a la qual aquest arriba
-    Real, dimension(N_d) :: Phi !Angle d'incidencia solar que depén de l'hora del dia (angle lateral)
+    Integer, parameter :: N_a = 365!Passos temporals (tot l'any)
+    Integer, parameter :: N_d = 288 !Passos temporals (tot el dia)
+    Real :: Theta (N_a) !Angle d'incidencia solar que depén de l'alçada a la qual aquest arriba discretitzarem per cada dia de l'any
+    Real :: Phi (N_d) !Angle d'incidencia solar que depén de l'hora del dia (angle lateral), discretitzarem cada 5 min
     Real :: H_i, H_f !Hora inicial i final d'incidencia de llum solar
-    Real, dimension(N_a) :: dist !Distancia terra - sol en cada dia de l'any
-    
+    Real :: dist(N_a) !Distancia terra - sol en cada dia de l'any
+    Real :: pos_sol !Posició del sol vist desde la placa solar
+
 
 End program mov_sol
